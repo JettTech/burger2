4a789c344038e9d3185f44f746aaa0f2373b5ff3
Lisa Jetton <ldjetton21@gmail.com>
2018-04-12 17:00:35 -0500

    server.js updates;

--- server.js
+++ server.js
@@ -1,36 +1,22 @@
-// Dependencies
-// ===========================================================
-var path = require("path");
-var bodyParser = require("body-parser");
 var express = require("express");
+var bodyParser = require("body-parser");
+var PORT = process.env.PORT || 8000;
 var app = express();
-var exphbs = require("express-handlebars");
 
-////////////// ***** NEW DEPENDENCIES TO ENSURE: ****** //////////////////
-var exphbs = require("express-handlebars");
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-
-app.use(express.static(__dirname + "/public")); //this offers the public folder as
-// local folder accessible to all files (the public folder becomes the new starting point for any files outside the folder to access any files within that folder.)
-
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(methodOverride("method"));//this method will allow the POST to override with "?_method=DELETE"
+var exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// DEFINITION OF THE PORT and ENGINE
-// ===========================================================
-var PORT = process.env.PORT || 4000;  //This will allow the port to work off of the local port envorionemnt, defaulting to the localhost port 4000.
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
-app.set("view engine", "handlebars"); //THIS IS (and the app.engine above) the EXPHBS NPM functionality/syntax
+var routes = require("./controllers/burgersController.js");
 
-// Routes
-// =========================================================== 
-var routes = require("./controllers/burgers_controller.js");
-app.use("/", routes); //this will refer to the var "routes" above, which requires in thh Controller JS file.
+app.use(routes);
 
-
-// Listener
-// ===========================================================
-app.listen(PORT, function(request, result) {
-	console.log("Hey there Developer!  You are listening on Port: " + PORT);
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
 });
